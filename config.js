90c024ffdc7a0b8bb92c773fba492f061d40a718
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-10-10 22:32:30 +0530

    cors issues found

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 export const corsOptions = {
-    origin: [
-        'http://localhost:5000',
-        'https://food-delivary-app-psi.vercel.app'
-    ],
-    credentials: true,
+  origin: [
+    "http://localhost:5000",
+    "http://localhost:3000",
+    "https://food-delivary-app-server.vercel.app",
+    "https://food-delivary-app-psi.vercel.app",
+  ],
+  credentials: true,
 };
