a1cde08431a340f819b53ac218cbdb0629cc5ffe
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-10-18 00:38:19 +0530

    server added users

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {corsOptions} from "./config.js";
-import {connectDB} from "./db.js"
+import { corsOptions } from "./config.js";
+import { connectDB } from "./db.js";
 import jwtRoutes from "./routes/jwt.js";
 import restaurentRoute from "./routes/restaurent.js";
 import menuRoute from "./routes/menu.js";
 import totalCountRoute from "./routes/totalCount.js";
-import deliveryRoute from "./routes/deliveryBoy.js"
+import deliveryRoute from "./routes/deliveryBoy.js";
+import totalUsers from "./routes/totalUsers.js";
 
 dotenv.config();
 
-
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ origin: corsOptions }));
 app.use(express.json());
 
 // Routes
-app.use('/jwt',jwtRoutes)
-app.use('/restaurents',restaurentRoute)
-app.use('/single-menu',menuRoute)
-app.use('/cart-menu',menuRoute)
-app.use('/cart-menu-delete',menuRoute)
-app.use('/menus',menuRoute)
-app.use('/delivery-man',deliveryRoute)
-
-
+app.use("/jwt", jwtRoutes);
+app.use("/restaurents", restaurentRoute);
+app.use("/single-menu", menuRoute);
+app.use("/cart-menu", menuRoute);
+app.use("/cart-menu-delete", menuRoute);
+app.use("/menus", menuRoute);
+app.use("/delivery-man", deliveryRoute);
+app.use("/users", totalUsers);
 
 // total information for (dashboard)
-app.use('/', totalCountRoute);
+app.use("/", totalCountRoute);
 
-app.get("/",(req, res)=>{
-    res.send('Food delivery server running')
-})
+app.get("/", (req, res) => {
+  res.send("Food delivery server running");
+});
 
-const run = async() =>{
-    await connectDB();
-    app.listen(port,()=>{
-        console.log(`Food delivery server port : ${port}`);
-    })
-}
+const run = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Food delivery server port : ${port}`);
+  });
+};
 
-run().catch(console.dir)
+run().catch(console.dir);
--- routes/restaurent.js
+++ routes/restaurent.js
@@ -1,78 +1,81 @@
 import express from "express";
-import {  restaurentCollection, restaurentFavoriteCollection, restaurentReviewsCollection } from "../db.js";
+import {
+  restaurentCollection,
+  restaurentFavoriteCollection,
+  restaurentReviewsCollection,
+} from "../db.js";
 import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-router.get('/rest', async (req, res) => {
-   
-    const result = await restaurentCollection.find().toArray();
-    res.send(result);
-})
-
-router.get('/', async (req, res) => {
-    const search = req.query.search;
-    let query = {};
-    if (search) {
-        query.restaurantName = {
-            $regex: search,
-            $options: "i",
-        };
-    }
-    const cursor = restaurentCollection.find(query);
-    const result = await cursor.toArray();
-    res.send({
-        restaurents: result
-    });
-})
-
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-
-    // Check if the id is a valid ObjectId
-    if (!ObjectId.isValid(id)) {
-        return res.status(400).send({ error: 'Invalid restaurant ID' });
-    }
-
-    const query = { _id: new ObjectId(id) };
-    const result = await restaurentCollection.findOne(query);
-    res.send(result);
+router.get("/rest", async (req, res) => {
+  const result = await restaurentCollection.find().toArray();
+  res.send(result);
 });
 
-router.post('/', async(req,  res)=>{
-    const restaurant = req.body
-   const result = await restaurentCollection.insertOne(restaurant)
-    res.send(result)
-}) 
+router.get("/", async (req, res) => {
+  const search = req.query.search;
+  let query = {};
+  if (search) {
+    query.restaurantName = {
+      $regex: search,
+      $options: "i",
+    };
+  }
+  const cursor = restaurentCollection.find(query);
+  const result = await cursor.toArray();
+  res.send({
+    restaurents: result,
+  });
+});
 
-router.get('/restReviews', async(req,  res)=>{
-    const name = req.params.title || "Spice Paradise"
-    const filter = {restaurantName : name}
-    const result = await restaurentReviewsCollection.find(filter).toArray()
-   console.log(result)
-    res.send(result)
-}) 
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
 
-router.post('/restReviews', async(req,  res)=>{
-    const review = req.body
-    const result = await restaurentReviewsCollection.insertOne(review)
-    res.send(result)
-}) 
+  // Check if the id is a valid ObjectId
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "Invalid restaurant ID" });
+  }
 
-router.get('/restFavorite', async(req,  res)=>{
-    const { userName, restaurantName } = req.query;
-  
-    const filter = {};
-    if (userName) filter.userName = userName;  // Ensure correct field in the MongoDB filter
-    if (restaurantName) filter.restaurantName = restaurantName;
+  const query = { _id: new ObjectId(id) };
+  const result = await restaurentCollection.findOne(query);
+  res.send(result);
+});
 
-    const result = await restaurentFavoriteCollection.find(filter).toArray();
-    res.send(result);
-}) 
+router.post("/", async (req, res) => {
+  const restaurant = req.body;
+  const result = await restaurentCollection.insertOne(restaurant);
+  res.send(result);
+});
 
-router.post('/restFavorite', async(req,  res)=>{
-    const favorite = req.body
-    const result = await restaurentFavoriteCollection.insertOne(favorite)
-    res.send(result)
-}) 
-export default router
+router.get("/restReviews", async (req, res) => {
+  const name = req.params.title || "Spice Paradise";
+  const filter = { restaurantName: name };
+  const result = await restaurentReviewsCollection.find(filter).toArray();
+  console.log(result);
+  res.send(result);
+});
+
+router.post("/restReviews", async (req, res) => {
+  const review = req.body;
+  const result = await restaurentReviewsCollection.insertOne(review);
+  res.send(result);
+});
+
+router.get("/restFavorite", async (req, res) => {
+  const { userName, restaurantName } = req.query;
+
+  const filter = {};
+  if (userName) filter.userName = userName; // Ensure correct field in the MongoDB filter
+  if (restaurantName) filter.restaurantName = restaurantName;
+
+  const result = await restaurentFavoriteCollection.find(filter).toArray();
+  res.send(result);
+});
+
+router.post("/restFavorite", async (req, res) => {
+  const favorite = req.body;
+  const result = await restaurentFavoriteCollection.insertOne(favorite);
+  res.send(result);
+});
+export default router;
