71b9fe27d3e00ba189ca67be43620b9a55c4d850
Antor4q <tariquelislam2015@gmail.com>
2024-10-30 17:04:33 +0600

    manage rider status update and delete api done

--- routes/deliveryBoy.js
+++ routes/deliveryBoy.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { deliveryManCollection } from "../db.js";
+import { deliveryManCollection, userCollection } from "../db.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router()
 
@@ -10,6 +11,35 @@ router.get('/', async(req,res) => {
 router.post('/', async(req, res) => {
     const item = req.body
     const result = await deliveryManCollection.insertOne(item)
+    res.send(result)
+})
+
+router.patch("/status", async(req,res)=>{
+    const email = req.body?.email
+    const query = { email : email}
+    const update = {
+        $set : {
+            status : "accepted"
+        }
+    }
+    const update2 = {
+        $set : {
+            role : "rider"
+        }
+    }
+   const result = await deliveryManCollection.updateOne(query,update)
+   const result2 = await userCollection.updateOne(query,update2)
+  
+   res.send({result,result2})
+})
+
+router.delete('/:id', async(req,res)=> {
+    const id = req.params.id
+   
+    const objectId = new ObjectId(id);
+    const query = { _id : objectId }
+   
+    const result = await deliveryManCollection.deleteOne(query)
     res.send(result)
 })
 export default router;
