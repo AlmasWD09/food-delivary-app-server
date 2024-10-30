3135f2a1096923c4877aced10db9424276e3ddc2
Antor4q <tariquelislam2015@gmail.com>
2024-10-30 12:37:11 +0600

    all riders get api creted

--- routes/deliveryBoy.js
+++ routes/deliveryBoy.js
@@ -3,6 +3,11 @@ import { deliveryManCollection } from "../db.js";
 
 
 const router = express.Router()
+
+router.get('/', async(req,res) => {
+    const result = await deliveryManCollection.find().toArray()
+    res.send(result)
+})
 router.post('/', async(req, res) => {
     const item = req.body
     const result = await deliveryManCollection.insertOne(item)
