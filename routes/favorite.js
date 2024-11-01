32e27c50a8bacdd2aadb09067237c2debceea019
Antor4q <tariquelislam2015@gmail.com>
2024-11-01 12:23:06 +0600

    favorite api filter updated

--- routes/favorite.js
+++ routes/favorite.js
@@ -4,12 +4,20 @@ import { restaurentFavoriteCollection } from "../db.js";
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const { userName, restaurantName } = req.query;
-
+    const { userEmail, restaurantName } = req.query;
+    console.log(userEmail,restaurantName)
     const filter = {};
-    if (userName) filter.userName = userName;  // Ensure correct field in the MongoDB filter
-    if (restaurantName) filter.restaurantName = restaurantName;
 
+    // Only add userEmail to the filter if it's provided
+    if (userEmail) {
+      filter.userEmail = userEmail;
+    }
+    
+    // Only add restaurantName to the filter if it's provided
+    if (restaurantName) {
+      filter.restaurantName = restaurantName;
+    }
+   
     const result = await restaurentFavoriteCollection.find(filter).toArray();
     res.send(result);
 })
